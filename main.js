18adfbd10605858aff46a6c67c35c41b893e5f36
Bice93 <bianca.cerulo@hotmail.it>
2022-07-04 14:16:49 +0200

    Print Message

---
+++ main.js
@@ -0,0 +1,10 @@
+//Stampare a schermo un messaggio all’interno di un elemento h1, utilizzando i data nell'istanza di Vue.
+
+const app = new Vue (
+    {
+        el:`#root`,
+        data: {
+            message : ``,
+        }
+    }
+);
